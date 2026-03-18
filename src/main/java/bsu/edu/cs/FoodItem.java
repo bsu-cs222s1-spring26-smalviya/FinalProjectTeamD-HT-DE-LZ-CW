bec772e9180994d1be85ae8e29fd42539791eefb
LooBaka <zhuyifan0717@gmail.com>
2026-03-18 18:01:42 -0400

    Added FoodItem and Getters

--- src/main/java/bsu/edu/cs/FoodItem.java
+++ src/main/java/bsu/edu/cs/FoodItem.java
@@ -13,18 +13,67 @@ public class FoodItem {
     private double sugar;
     private double fiber;
     private double carbs;
-    private double cholestero;
+    private double cholesterol;
 
-    public FoodItem(String datum) {
-
+    public FoodItem(String foodName) {
+        this.foodName = foodName;
+        this.calories = 0.0;
+        this.potassium = 0.0;
+        this.iron = 0.0;
+        this.fat = 0.0;
+        this.protein = 0.0;
+        this.calcium = 0.0;
+        this.sugar = 0.0;
+        this.fiber = 0.0;
+        this.carbs = 0.0;
+        this.cholesterol = 0.0;
     }
 
-    public Object getCalories() {
+    public String getFoodName() {
+        return foodName;
     }
 
-    public String getProtein() {
+    public double getCalories() {
+        return calories;
     }
 
-    public String getCarbs() {
+    public double getPotassium() {
+        return potassium;
+    }
+
+    public double getIron() {
+        return iron;
+    }
+
+    public double getFat() {
+        return fat;
+    }
+
+    public double getProtein() {
+        return protein;
+    }
+
+    public double getCalcium() {
+        return calcium;
+    }
+
+    public double getSugar() {
+        return sugar;
+    }
+
+    public double getFiber() {
+        return fiber;
+    }
+
+    public double getCarbs() {
+        return carbs;
+    }
+
+    public double getCholesterol() {
+        return cholesterol;
+    }
+
+    public String getNutrition() {
+        return foodName + "\nCalories: " + calories + "\nProtein: " + protein + "g\nCarbs: " + carbs + "g\nFat: " + fat + "g";
     }
 }
