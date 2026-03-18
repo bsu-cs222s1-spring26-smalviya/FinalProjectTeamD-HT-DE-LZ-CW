2a92679a82686db27f5d581920d7ee64dff6e864
LooBaka <zhuyifan0717@gmail.com>
2026-03-18 14:41:33 -0400

    Added needed variables in FoodItem

--- src/main/java/bsu/edu/cs/FoodItem.java
+++ src/main/java/bsu/edu/cs/FoodItem.java
@@ -1,7 +1,22 @@
 package bsu.edu.cs;
 
 public class FoodItem {
+
+    //Creating private variables so data won't conflict with other classes
+    private String foodName;
+    private double calories;
+    private double potassium;
+    private double iron;
+    private double fat;
+    private double protein;
+    private double calcium;
+    private double sugar;
+    private double fiber;
+    private double carbs;
+    private double cholesterol;
+
     public FoodItem(String datum) {
+
     }
 
     public Object getCalories() {
