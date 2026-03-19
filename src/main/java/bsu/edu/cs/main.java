ab96007d747a237e400d8f02f17b56c5cf2363db
H05T3 <humbertolllt@gmail.com>
2026-03-19 11:53:58 -0400

    Updated main; Will make easier to make a interface Class

--- src/main/java/bsu/edu/cs/main.java
+++ src/main/java/bsu/edu/cs/main.java
@@ -50,17 +50,13 @@ public class main {
         boolean running = true;
         while(running) {
             //More options to come later like recipe search and all that
-            System.out.print("\nHere are your options for the logger: \n" +
-                    "Type any of the numbers below\n" +
-                    "1. Get Food info\n" +
-                    "2. Log Food\n" +
-                    "3. Get user food log for today\n" +
-                    "Input number here (or type 0 to quit): ");
+            System.out.print(mainMenu());
             int userResponse = scanner.nextInt();
             switch (userResponse){
                 case 0:
                     System.out.println("quitting program....");
                     running = false;
+                    break;
                 case 1:
                     //Later we will make it so the user can input the item and we will find the foods
                     System.out.println(dataQuery.displayFoodData("rice"));
@@ -74,7 +70,16 @@ public class main {
                     //Later we will make it so it shows the days that the user has logged and they can choose
                     //For testing cases tho this will be the date we choose.
                     dataQuery.grabUserLogForDay(12,07,25);
+                    break;
             }
         }
+    }
+    private static String mainMenu(){
+        return "\nHere are your options for the logger: \n" +
+                "Type any of the numbers below\n" +
+                "1. Get Food info\n" +
+                "2. Log Food\n" +
+                "3. Get user food log for today\n" +
+                "Input number here (or type 0 to quit): ";
     }
 }
