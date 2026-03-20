efc28adce7c6e62eb352152459164e32a67381cc
LooBaka <zhuyifan0717@gmail.com>
2026-03-20 14:06:29 -0400

    Added FoodItem test class

---
+++ src/test/java/bsu/edu/cs/FoodItemTest.java
@@ -0,0 +1,26 @@
+package bsu.edu.cs;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FoodItemTest {
+
+    @Test
+    public void testFoodItemConstructor() {
+        FoodItem testApple = new FoodItem("rice");
+
+        assertEquals("rice", testApple.getFoodName(), "The name should be 'rice'");
+
+        assertEquals(0.0, testApple.getCalories(), "Default calories should be 0.0");
+        assertEquals(0.0, testApple.getProtein(), "Default protein should be 0.0");
+    }
+
+    @Test
+    public void testGetNutritionOutput() {
+        FoodItem testBanana = new FoodItem("Banana");
+
+        String expectedOutput = "Banana\nCalories: 0.0\nProtein: 0.0g\nCarbs: 0.0g\nFat: 0.0g";
+
+        assertEquals(expectedOutput, testBanana.getNutrition(), "The nutrition summary string is not correct.");
+    }
+}
