0dfd1464c3a48ea4ca0f940947d58d6cc732bcfe
LooBaka <zhuyifan0717@gmail.com>
2026-04-01 18:22:47 -0400

    finalize FoodItem and integrate USDAListParser

--- src/main/java/bsu/edu/cs/foodData/FoodItem.java
+++ src/main/java/bsu/edu/cs/foodData/FoodItem.java
@@ -1,10 +1,12 @@
 package bsu.edu.cs.foodData;
 
 import bsu.edu.cs.APIApps.USDAClient.USDAParser;
+import bsu.edu.cs.APIApps.USDAClient.USDAListParser;
 
 public class FoodItem {
 
     private USDAParser parser = new USDAParser();
+    private USDAListParser listParser = new USDAListParser();
     private String foodName;
     private int fdcID;
     private double calories;
@@ -19,18 +21,33 @@ public class FoodItem {
     private double cholesterol;
 
     public FoodItem() {
-        this.foodName = "Rice";
-        this.fdcID = 0;
-        this.calories = 130.0;
-        this.potassium = 0.0;
-        this.iron = 0.0;
-        this.fat = 0.0;
-        this.protein = 2.7;
-        this.calcium = 0.0;
-        this.sugar = 0.0;
-        this.fiber = 0.0;
-        this.carbs = 28.2;
-        this.cholesterol = 0.0;
+        try {
+            this.foodName = listParser.parseForNameofFood(1);
+            this.fdcID = listParser.parseForFDCID(1);
+            this.calories = parser.parseForCalories();
+            this.potassium = parser.parseForPotassium();
+            this.iron = parser.parseForIron();
+            this.fat = parser.parseForSatFat() + parser.parseForUnSatFat();
+            this.protein = parser.parseForProtein();
+            this.calcium = parser.parseForCalcium();
+            this.sugar = parser.parseForSugar();
+            this.fiber = parser.parseForFiber();
+            this.carbs = parser.parseForCarbs();
+            this.cholesterol = parser.parseForCholesterol();
+        } catch (Exception e) {
+            this.foodName = "Rice";
+            this.fdcID = 0;
+            this.calories = 130.0;
+            this.potassium = 0.0;
+            this.iron = 0.0;
+            this.fat = 0.0;
+            this.protein = 2.7;
+            this.calcium = 0.0;
+            this.sugar = 0.0;
+            this.fiber = 0.0;
+            this.carbs = 28.2;
+            this.cholesterol = 0.0;
+        }
     }
 
     public String getFoodName() {
