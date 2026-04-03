41c295e9129411cf92a9c8199f74f52648a87f65
Dgooden83 <41882896+bgooden@users.noreply.github.com>
2026-04-02 20:47:27 -0400

    MacroCalc.java

--- src/main/java/bsu/edu/cs/calculators/MacroCalc.java
+++ src/main/java/bsu/edu/cs/calculators/MacroCalc.java
@@ -1,4 +1,9 @@
 package bsu.edu.cs.calculators;
-
+import java.util.Scanner;
 public class MacroCalc {
+  public static void main(String[] args) {
+    Scanner scanner = new scanner(System.in)
+
+      
+  }
 }
