b476c258bbb3d800b7cf11f359f1c3c9e6795749
Dgooden83 <41882896+bgooden@users.noreply.github.com>
2026-04-03 22:01:30 -0400

    MacroCalc.java

--- src/main/java/bsu/edu/cs/calculators/MacroCalc.java
+++ src/main/java/bsu/edu/cs/calculators/MacroCalc.java
@@ -2,8 +2,12 @@ package bsu.edu.cs.calculators;
 import java.util.Scanner;
 public class MacroCalc {
   public static void main(String[] args) {
-    Scanner scanner = new scanner(System.in)
 
+    double tdee = bmr * 1.55;
+    
+    double protein = (tdee * 0.30) / 4;
+    double fat = (tdee * 0.30) / 9;
+    double carbs = (tdee * 0.40)/4;
       
   }
 }
